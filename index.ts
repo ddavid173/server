2bad05163cdea465f61214c35bfc01d2cd851c09
ddavid173 <davidadelgado08@gmail.com>
2023-09-27 11:48:59 -0500

    Bun base

--- index.ts
+++ index.ts
@@ -1,9 +1,16 @@
-const server = Bun.serve({
-  hostname: "::",
-  port: process.env.PORT ?? 3000,
-  fetch(request) {
-    return new Response("Welcome to Bun!");
-  },
-});
+import App from 'bunrest'
+const server = App();
+const port = 3000
 
-console.log(`Listening on http://localhost:${server.port}`);
+server.get('/', (req, res) => {
+  res.status(200).send("It worked")
+})
+
+server.get('/favicon.ico', (req, res) => {
+  res.status(400).json({error: 'No favicon avalible'})
+})
+
+server.listen(`${port}`, () => {
+  console.log(`App is listening on port ${port}`)
+})
+
